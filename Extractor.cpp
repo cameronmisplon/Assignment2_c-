5c4a0eba44bc7a4e078424463beeca006eba5aa0
Cameron Misplon <xdeecsgo@gmail.com>
2021-04-11 09:41:24 +0200

    Moved gradient calculator into its own function of the FrameSequence class to make the driver function more modular

--- Extractor.cpp
+++ Extractor.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <cstring>
 #include "FrameSequence.h"
-#include <math.h>
 
 int main(int argc, char* argv[])
 {
@@ -12,7 +11,7 @@ int main(int argc, char* argv[])
 	if (in){
 		std::string pgm,comments,sizedata,brightness,origins,framesize;
 		int Ncols, Nrows,framex,framey;
-		float gradient,xend,xcoord,yend,ycoord;
+		float xend,xcoord,yend,ycoord;
 		MSPCAM001::FrameSequence frames;
 		getline(in,pgm);
 		getline(in,comments);
@@ -35,66 +34,7 @@ int main(int argc, char* argv[])
 		iss >> framex >> framey;
 		frames.setSize(framex,framey,Ncols,Nrows);
 			
-		gradient = (float)(yend-ycoord)/float(xend-xcoord);
-		frames.read(pixels,xcoord,ycoord);
-		if (fabs(gradient)<=1.0)
-		{
-			if(xcoord <xend){
-				for(int i=xcoord+1;i<=xend; i++)
-				{
-					if (ycoord < yend)
-					{
-						ycoord+=fabs(gradient);
-					}
-					else{
-						ycoord-=fabs(gradient);
-					}
-					frames.read(pixels,i,std::round(ycoord));
-				}
-			}
-			else{
-				for(int j=xcoord-1;j>=xend; j--)
-				{
-					if (ycoord < yend)
-					{
-						ycoord+=fabs(gradient);
-					}
-					else{
-						ycoord-=fabs(gradient);
-					}
-					frames.read(pixels,j,std::round(ycoord));
-				}
-			}
-		}
-		else{
-			gradient = (float)(xend-xcoord)/(float)(yend-ycoord);
-			if(ycoord <yend){
-				for(int k=ycoord+1;k<=yend; k++)
-				{
-					if (xcoord < xend)
-					{
-						xcoord+=fabs(gradient);
-					}
-					else{
-						xcoord-=fabs(gradient);
-					}
-					frames.read(pixels,std::round(xcoord),k);
-				}
-			}
-			else{
-				for(int l=ycoord-1;l>=yend; l--)
-				{
-					if (xcoord < xend)
-					{
-						xcoord+=fabs(gradient);
-					}
-					else{
-						xcoord-=fabs(gradient);
-					}
-					frames.read(pixels,std::round(xcoord),l);
-				}
-			}
-		}	
+		frames.gradientCalculator(xcoord,ycoord,xend,yend,pixels);	
 		delete [] pixels;
 	}
 	in.close();	
--- FrameSequence.cpp
+++ FrameSequence.cpp
@@ -1,7 +1,7 @@
 #include "FrameSequence.h"
 
 namespace MSPCAM001{
-	MSPCAM001::FrameSequence::FrameSequence(): frameSizeX(0) , frameSizeY(0)
+	MSPCAM001::FrameSequence::FrameSequence(): frameSizeX(0) , frameSizeY(0), imageSizeX(0), imageSizeY(0), gradient(0)
 	{}
 	MSPCAM001::FrameSequence::~FrameSequence()
 	{
@@ -40,5 +40,69 @@ namespace MSPCAM001{
 		}
 		(this->imageSequence).push_back(frame);
 	}
+	void MSPCAM001::FrameSequence::gradientCalculator(int xcoord, int ycoord, int xend, int yend, unsigned char * pixels)
+	{
+		gradient = (float)(yend-ycoord)/float(xend-xcoord);
+		read(pixels,xcoord,ycoord);
+		if (fabs(gradient)<=1.0)
+		{
+			if(xcoord <xend){
+				for(int i=xcoord+1;i<=xend; i++)
+				{
+					if (ycoord < yend)
+					{
+						ycoord+=fabs(gradient);
+					}
+					else{
+						ycoord-=fabs(gradient);
+					}
+					read(pixels,i,std::round(ycoord));
+				}
+			}
+			else{
+				for(int j=xcoord-1;j>=xend; j--)
+				{
+					if (ycoord < yend)
+					{
+						ycoord+=fabs(gradient);
+					}
+					else{
+						ycoord-=fabs(gradient);
+					}
+					read(pixels,j,std::round(ycoord));
+				}
+			}
+		}
+		else{
+			gradient = (float)(xend-xcoord)/(float)(yend-ycoord);
+			if(ycoord <yend){
+				for(int k=ycoord+1;k<=yend; k++)
+				{
+					if (xcoord < xend)
+					{
+						xcoord+=fabs(gradient);
+					}
+					else{
+						xcoord-=fabs(gradient);
+					}
+					read(pixels,std::round(xcoord),k);
+				}
+			}
+			else{
+				for(int l=ycoord-1;l>=yend; l--)
+				{
+					if (xcoord < xend)
+					{
+						xcoord+=fabs(gradient);
+					}
+					else{
+						xcoord-=fabs(gradient);
+					}
+					read(pixels,std::round(xcoord),l);
+				}
+			}
+		}
+	}
+		
 }	
 			
