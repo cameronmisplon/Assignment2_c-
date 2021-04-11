50d54c5d3742bfba0201aa5bdfdfe63618c15045
Cameron Misplon <mspcam001@nightmare.cs.uct.ac.za>
2021-04-11 17:35:58 +0200

    Added write function which executes based on option chosen, and added support for -p option

--- Extractor.cpp
+++ Extractor.cpp
@@ -7,10 +7,10 @@
 
 int main(int argc, char* argv[])
 {
-	std::ifstream in("sloan_image.pgm", std::ios::binary | std::ios::in);
+	std::ifstream in(std::string(argv[1]), std::ios::binary | std::ios::in);
 	if (in){
-		std::string pgm,comments,sizedata,brightness,origins,framesize;
-		int Ncols, Nrows,framex,framey;
+		std::string pgm,comments,sizedata,brightness,origins,framesize,filename,option;
+		int Ncols, Nrows,framex,framey,polyLine,polyLineMultiplier;
 		float xend,xcoord,yend,ycoord;
 		MSPCAM001::FrameSequence frames;
 		getline(in,pgm);
@@ -21,21 +21,44 @@ int main(int argc, char* argv[])
 		getline(in,brightness);
 		unsigned char * pixels = new unsigned char[Ncols*Nrows]; //Use unsigned char because it has range 0-255 which is good for the pixel values
 		in.read(reinterpret_cast<char *>(pixels),Ncols*Nrows);
+		unsigned char * temp = &(pixels[10]);
+		unsigned char v = 255;
+	        unsigned char temp2 = static_cast<unsigned char>(v - *temp);
+		std::cout << static_cast<float>(temp2) << std::endl;	
 			
-		iss.str("");
-		iss.seekg(0);
-		origins = std::string(argv[2]) +" "+ std::string(argv[3]) +" "+ std::string(argv[4]) +" "+ std::string(argv[5]);
-		iss.str(origins);
-		iss >> xcoord >> ycoord >> xend >>yend;
-		iss.str("");
-		iss.seekg(0);
-		framesize = std::string(argv[7])+" "+std::string(argv[8]);
-		iss.str(framesize);
-		iss >> framex >> framey;
-		frames.setSize(framex,framey,Ncols,Nrows);
-			
-		frames.gradientCalculator(xcoord,ycoord,xend,yend,pixels);	
+		std::string polylinechoice = std::string(argv[2]);
+		polyLine=0;
+		polyLineMultiplier=2;
+		if (polylinechoice.compare("-p")==0){
+			iss.str("");
+			iss.seekg(0);
+			iss.str(std::string(argv[3]));
+			iss >> polyLineMultiplier;
+			std::cout << polyLineMultiplier << std::endl;
+			polyLine=1;
+		}
+		for (int i=0 ;i<polyLineMultiplier-1;i++)
+		{
+			iss.str("");
+			iss.seekg(0);
+			origins = std::string(argv[(2*i)+3+polyLine]) +" "+ std::string(argv[(2*i)+4+polyLine]) +" "+ std::string(argv[(2*i)+5+polyLine]) +" "+ std::string(argv[(2*i)+6+polyLine]);
+			iss.str(origins);
+			iss >> xcoord >> ycoord >> xend >>yend;
+			iss.str("");
+			iss.seekg(0);
+			framesize = std::string(argv[(polyLineMultiplier*2)+4+polyLine])+" "+std::string(argv[(polyLineMultiplier*2)+5+polyLine]);
+			iss.str(framesize);
+			iss >> framex >> framey;
+			frames.setSize(framex,framey,Ncols,Nrows);
+			frames.gradientCalculator(xcoord,ycoord,xend,yend,pixels);
+		}
 		delete [] pixels;
+		for (int k=((polyLineMultiplier*2)+6+polyLine);k<argc;k+=3)
+		{
+			option = std::string(argv[k+1]);
+			filename = std::string(argv[k+2]);
+			frames.write(filename,option);
+		}
 	}
 	in.close();	
 	return 0;
--- FrameSequence.h
+++ FrameSequence.h
@@ -3,17 +3,18 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <math.h>
+#include <fstream>
 
 namespace MSPCAM001{
 	class FrameSequence{
 		std::vector<unsigned char **> imageSequence;
 		int frameSizeX,frameSizeY,imageSizeX,imageSizeY;
-		float gradient;
 		public:
 		FrameSequence();
 		~FrameSequence();
-		// void write(std::string filename, std::string option std::string pgm, std::string comments);
+		void write(std::string filename, std::string option);
 		void setSize(int framex, int framey, int imagex, int imagey);
 		void read(unsigned char *pixels,int originx, int originy);
 		void gradientCalculator(int xcoord, int ycoord, int xend, int yend, unsigned char * pixels);
